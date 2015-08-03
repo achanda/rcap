577cbfebd1c011cc9d45fd0d6ad75109144c0b1c
chanda, abhishek <abhishek.chanda@emc.com>
2015-08-02 21:55:14 -0700

    We don't need attributes in internal module

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@
 # usage build.sh <path_to_cap_file>
 out_file="src/caps.rs"
 caps=$(sed -n 's/^#define \(CAP_.*\) .*/\1/p' "$1" | head -n -2 | tr -d ' ' | sed -e 's/$/,/g' )
-echo "#![crate_name = \"rcap\"]" > $out_file
-echo "#![crate_type = \"dylib\"]" >> $out_file
 echo "pub enum Caps {" >> $out_file
 echo "$caps" >> $out_file
 echo "}" >> $out_file
