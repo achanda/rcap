e9f8d749c76df5b3cf5a4340d8f2f84cc90c10c8
chanda, abhishek <abhishek.chanda@emc.com>
2015-08-02 21:47:30 -0700

    Tweak build script a bit

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # usage build.sh <path_to_cap_file>
-caps=$(sed -n 's/^#define \(CAP_.*\) .*/\1/p' "$1" | head -n -2 | tr "" "\n")
-echo "$caps" > "src/caps.rs"
+out_file="src/caps.rs"
+caps=$(sed -n 's/^#define \(CAP_.*\) .*/\1/p' "$1" | head -n -2 | tr -d ' ' | sed -e 's/$/,/g' )
+echo "#![crate_name = \"rcap\"]" > $out_file
+echo "#![crate_type = \"dylib\"]" >> $out_file
+echo "pub enum Caps {" >> $out_file
+echo "$caps" >> $out_file
+echo "}" >> $out_file
