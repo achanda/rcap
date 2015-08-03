e7632eb8234fd20de5adc8f8214213c1073f44bd
chanda, abhishek <abhishek.chanda@emc.com>
2015-08-02 21:56:54 -0700

    Delete the outfile if it exists

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 # usage build.sh <path_to_cap_file>
 out_file="src/caps.rs"
+if [ -f $out_file ] ; then
+    rm $out_file
+fi
 caps=$(sed -n 's/^#define \(CAP_.*\) .*/\1/p' "$1" | head -n -2 | tr -d ' ' | sed -e 's/$/,/g' )
 echo "pub enum Caps {" >> $out_file
 echo "$caps" >> $out_file
